9eb50f70af4a1360aae8e617672d69dd29c56d76
Emma Gheysen <58299316+gheysenemma@users.noreply.github.com>
2021-03-16 10:21:28 +0100

    export

--- R/rmse_sample.R
+++ R/rmse_sample.R
@@ -1,3 +1,12 @@
+#' @title Lowest-RMSE sample from time series
+#' @description Computes the Root Mean Square Error between subsequent generations (columns)
+#' in given species abundances matrix (time series) as a measure of convergence.
+#' The timepoint with the lowest RMSE-difference with respect to the previous timepoint
+#' is then extracted and a normalised sample is returned (proportions).
+#' @param spab Species abundances matrix with OTUs in the rows and the timepoints as columns.
+#' @return A sample vector with the length equal to the number of rows of given input species abundances matrix.
+#' @export
+
 
 rmse_sample <- function(spab){
   spab <- spab/colSums(spab)
