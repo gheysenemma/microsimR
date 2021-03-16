e7265744573d9f822f84788ec0417660f884897e
Emma Gheysen <58299316+gheysenemma@users.noreply.github.com>
2021-03-16 09:18:56 +0100

    glv without perturbation

--- R/glv.R
+++ R/glv.R
@@ -1,58 +1,35 @@
 #' @title Simulate time series with the generalized Lotka-Volterra model
 #'
 #' @description Simulate a community time series using the generalized Lotka-Volterra model, defined as
-#' \eqn{\frac{dx}{dt}=x(pbirth+Ax)}, where x is the vector of species abundances, A is the interaction matrix
-#' and pbirth the vector of growth rates.
+#' \eqn{\frac{dx}{dt}=x(growth_rates+Ax)}, where x is the vector of species abundances, A is the interaction matrix
+#' and growth_rates the vector of growth rates.
 #'
 #' @param N species number
 #' @param A interaction matrix
-#' @param pbirth growth rates
+#' @param growth_rates growth rates
 #' @param com initial abundances
-#' @param tstart initial time point
 #' @param tend final time point
-#' @param tstep time step
-#' @param perturb perturbation object describing growth changes
 #' @return a matrix with species abundances as rows and time points as columns, column names give time points
-#' @seealso \code{\link{ricker}} for the Ricker model
-#' @examples
-#' tsplot(glv(N=4,generateA(4)),header="gLV")
 #' @export
 
-glv<-function(N=4,A,pbirth=runif(N),com=runif(N),tstart=0,tend=100,tstep=0.1, perturb=NULL){
+glv<-function(N=4,A,growth_rates=runif(N),com=runif(N),tend=100){
   # checks perturb object and includes in parms
   # parms as matrix
-  parms=cbind(pbirth,A)
-  parms=cbind(rep(N,N),parms)
-  if (!is.null(perturb)){
-    if (length(perturb$growthchanges) != N){
-      stop("Please provide as many growth changes as species!")
-    }
-    else {
-      growthchanges = perturb$growthchanges
-      parms = cbind(parms,growthchanges)
-      if (length(perturb$times) != 0){
-        if (length(perturb$times) > N){
-          stop("Number of perturbations needs to be less than the number of species!")
-        }
-        else {
-          count = length(perturb$times)
-          times = vector(length = N, mode="numeric")
-          times[1:(length(perturb$times))] = perturb$times
-          durations = vector(length = N, mode="numeric")
-          durations[1:(length(perturb$durations))] = perturb$durations
-          parms = cbind(parms, times, durations)
-          parms = cbind(parms, rep(count, N))
-        }
-      }
-    }
-  }
-  times<-seq(tstart, tend, by=tstep)
+  parms = cbind(rep(N,N), growth_rates, A)
+  times<-seq(0, tend, by=1)
   # run the simulation
-  commtime<-lsoda(com, times, glvsolve, parms)
-  time=commtime[,1]
-  commtime=commtime[,2:ncol(commtime)]
-  commtime=t(commtime)
-  colnames(commtime)=time
+  commtime<-lsoda(
+    y = com, # initial state values for the ODE system
+    times = times, # times at which the explicit estimates for com are desired
+    func = glvsolve, # # function that computes the values of the derivatives in the ODE system
+    parms = parms, # parameters used in func
+    hmax = 0, # 0: no maximal integration stepsize specified
+    maxsteps = 500000, # max nr of steps per output interval taken by the solver
+  )
+  #time=commtime[,1]
+  commtime=t(commtime[,2:ncol(commtime)])
+  colnames(commtime)=paste0("t", 1:ncol(commtime))
+  rownames(commtime)=paste0("sp", 1:N)
   return(commtime)
 }
 
@@ -67,27 +44,8 @@ glv<-function(N=4,A,pbirth=runif(N),com=runif(N),tstart=0,tend=100,tstep=0.1, pe
 #
 glvsolve<-function(t, com, parms){
   N=parms[1,1]  # species number
-  pbirth=parms[,2]   # vector of growth rates
+  growth_rates=parms[,2]   # vector of growth rates
   A=parms[,3:(N+2)] # interaction matrix
-  if (length(parms[1,]) == (N+3)){
-    c=parms[,(N+3)]
-    pbirth = pbirth + c
-  }
-  else if (length(parms[1,]) > (N+3)){
-    c=parms[,(N+3)]
-    count = parms[1,(N+6)]
-    times = parms[,(N+4)]
-    durations = parms[,(N+5)]
-    intervals = list()
-    for (i in 1:count){
-      tstart = times[i]
-      tend = times[i] + durations[i]
-      if ((t>tstart) & (t<tend)){
-        pbirth = pbirth + c
-        #print(pbirth)
-      }
-    }
-  }
-  dydt <- com*(pbirth+A %*% com)
+  dydt <- com*(growth_rates+A %*% com)
   list(dydt)
 }
