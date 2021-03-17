86a98895dc5bb5d98ebd97ef69d9036d2fe35073
Emma Gheysen <58299316+gheysenemma@users.noreply.github.com>
2021-03-17 10:38:38 +0100

    glv simplification

--- R/glv.R
+++ R/glv.R
@@ -1,51 +1,56 @@
 #' @title Simulate time series with the generalized Lotka-Volterra model
 #'
 #' @description Simulate a community time series using the generalized Lotka-Volterra model, defined as
-#' \eqn{\frac{dx}{dt}=x(growth_rates+Ax)}, where x is the vector of species abundances, A is the interaction matrix
+#' dx/dt = x(b+Ax), where x is the vector of species abundances, A is the interaction matrix
 #' and growth_rates the vector of growth rates.
 #'
 #' @param N species number
 #' @param A interaction matrix
-#' @param growth_rates growth rates
-#' @param com initial abundances
-#' @param tend final time point
+#' @param b growth rates
+#' @param x initial abundances
+#' @param tend timepoints
+#' @param norm return normalised abundances (proportions in each generation)
 #' @return a matrix with species abundances as rows and time points as columns, column names give time points
+#' @examples glv(N = 4, A = powerlawA(n = 4, alpha = 2), tend = 1000)
+#' @note Calls upon the lsoda function from package deSolve to solve the ODE. If function crashes, consider running again with while loop and tryCatch handlings.
+#' @seealso deSolve::lsoda
 #' @export
 
-glv<-function(N=4,A,growth_rates=runif(N),com=runif(N),tend=100){
-  # checks perturb object and includes in parms
-  # parms as matrix
-  parms = cbind(rep(N,N), growth_rates, A)
-  times<-seq(1, tend, by=1)
-  # run the simulation
-  commtime<-lsoda(
-    y = com, # initial state values for the ODE system
-    times = times, # times at which the explicit estimates for com are desired
-    func = glvsolve, # # function that computes the values of the derivatives in the ODE system
-    parms = parms, # parameters used in func
-    hmax = 0, # 0: no maximal integration stepsize specified
-    maxsteps = 500000, # max nr of steps per output interval taken by the solver
+glv <- function(
+  N,
+  A,
+  b = runif(N),
+  x = runif(N),
+  tend = 1000,
+  norm = FALSE
+){
+  # 1 Model specification
+  # Model parameters
+  parameters <- cbind(b, A)
+
+  # 2 Model application
+  # Time specification
+  times <- seq(0, tend, by = 1)
+  # Model integration
+  out <- ode(
+    y = x,
+    times = times,
+    func = dxdt,
+    parms = parameters
   )
-  #time=commtime[,1]
-  commtime=t(commtime[,2:ncol(commtime)])
-  colnames(commtime)=paste0("t", 1:ncol(commtime))
-  rownames(commtime)=paste0("sp", 1:N)
-  return(commtime)
+  spab <- t(out[,2:ncol(out)])
+  if(norm){
+    spab <- spab/colSums(spab)
+  }
+  return(spab)
 }
 
-# ==============================================================
-# Equations (generalized Lotka-Volterra)
-# ==============================================================
-
-# matrix formulation of the ODE set
-# t: current simulation time
-# com: vector with current values of state variables (initial conditions)
-# parms: parameter values
-#
-glvsolve<-function(t, com, parms){
-  N=parms[1,1]  # species number
-  growth_rates=parms[,2]   # vector of growth rates
-  A=parms[,3:(N+2)] # interaction matrix
-  dydt <- com*(growth_rates+A %*% com)
-  list(dydt)
+# Model equations
+dxdt <- function(t, x, parameters){
+  b <- parameters[,1]
+  A <- parameters[,2:ncol(parameters)]
+  # rate of change
+  dx <- x*(b+A %*% x)
+  # return rate of change
+  list(dx)
 }
--- R/microsim.R
+++ R/microsim.R
@@ -1,6 +1,6 @@
 #' microsim package
 #'
-#' (For the moment an empty package)
+#' Simulator Package with a collection of basic community models that outputs count data of microbial communities.
 #'
 #' @docType package
 #'
@@ -9,7 +9,7 @@
 #' @name microsim
 #'
 #' @importFrom stats runif rmultinom rbinom rgamma rnorm
-#' @importFrom deSolve lsoda
 #' @importFrom poweRlaw rplcon
 #' @importFrom Metrics rmse
+#' @importFrom deSolve ode
 NULL
